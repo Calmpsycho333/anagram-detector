e1b2e5686f9b4ec94025ea2549c92b710aa9ce9e
calmpsycho333 <wrathking75@yahoo.com>
2020-11-21 13:30:23 -0500

    commit tree 6c4f8d0b99be05b48eaba07f950646036073c41d

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+
+class Anagram
+    attr_accessor :matches
+    def initialize(diaper)
+        @matches = diaper
+    end
+    def match(array)
+        array.select do | weirdo|
+            weirdo.chars.sort == @matches.chars.sort
+        end
+    end
+end
